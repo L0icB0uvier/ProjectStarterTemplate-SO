faf99b79bd3f7fa68a6814c193bbdce61b9a7f66
Loïc Bouvier <loic.bouvier@protonmail.com>
2022-09-02 18:06:56 +0200

    Add cold start up

---
+++ Assets/Scripts/ScriptableObjects/EventChannels/LocationChangeEventChannel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ScriptableObjects.EventChannels
+{
+    [CreateAssetMenu(menuName = "Events/Location Changed Event Channel")]
+    public class LocationChangeEventChannel : ScriptableObject
+    {
+        public UnityAction<LocationSO> OnLoadingRequested;
+
+        public void RaiseEvent(LocationSO locationToLoad)
+        {
+            OnLoadingRequested?.Invoke(locationToLoad);
+        }
+    }
+}
